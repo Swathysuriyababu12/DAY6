f586d4c9de0673895d8997326fe4fc0adc20f06e
Swathysuriyababu12 <swathysuryababu@gmail.com>
2022-03-18 19:19:35 +0530

    day6

---
+++ script.js
@@ -0,0 +1,99 @@
+//alert("“I’m invoked!”"); //quotes
+
+// let admin=9, fname=10.5; 
+// fname = "Guvi";
+// lname = "geek"
+// admin = fname+lname;
+// alert( admin ); // "Guvi geek"
+
+// let fname=10.5; 
+//  fname = "Guvi";
+//  lname = "geek";
+//  let name = fname+lname;
+// alert($name);
+
+// console.log("hello");
+// alert(3 +1 +3);
+// let fname=10.5; 
+// fname = "Guvi";
+// lname = "geek";
+// let name = fname+lname;
+// alert( `hello ${name}` );
+
+// let a = parseInt(prompt("First number?"));
+// let b = parseInt(prompt("Second number?"));
+// alert(a + b);
+
+// var a = "2" > "12";//Don't touch below this
+// if (a) {
+//   console.log("Code is Blasted")
+// }
+// else
+// {
+//   console.log("Diffused") 
+// }
+
+
+// let a = prompt("Enter a number?");//Don't modify any code below this
+// if (a) {
+//  console.log( 'OMG it works for any number inc 0' );
+// }
+// else
+// {
+//  console.log( "Success" );
+// }
+
+// let value = prompt('How many runs you scored in this ball');
+// if (value == 4) {
+//       console.log("You hit a Four");
+// } else if (value == 6) {
+//       console.log("You hit a Six");
+// } else {
+//       console.log("I couldn't figure out");
+// }
+
+// let login = 'Employee';
+// let message = (login == 'Employee') ?
+//   (login == 'Director') ? 'Greetings' :
+//   (login == '') ? 'No login' :'welocome':'';
+//   console.log(message);
+
+
+//   let  message;
+//   if (null || 2 || undefined )
+//   {
+//      message = "welcome boss";
+//   }
+//   else
+//   {
+//      message = "Go away";
+//   }
+// console.log(message);
+
+// let i = 3;
+// while (i) 
+// {
+//   console.log( i-- );
+// }
+
+// let num=1;
+// while(num<=10){
+//     console.log(num);
+//     num++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
